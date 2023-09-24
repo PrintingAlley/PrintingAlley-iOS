d5f4fd4357386f54a5fae6fa828121bd3288242f
Hamp <yongbeomkwak@gmail.com>
2023-09-24 15:55:24 +0900

    :film_projector: :: 크러쉬 스크립트 설정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,7 +6,7 @@ let dependencies = Dependencies(
     swiftPackageManager: SwiftPackageManagerDependencies(
         [
             .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.24.0")),
-            .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .upToNextMajor(from: "10.7.0"))
+            .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .upToNextMajor(from: "10.15.0"))
         ],
         baseSettings: .settings(
             configurations: [
