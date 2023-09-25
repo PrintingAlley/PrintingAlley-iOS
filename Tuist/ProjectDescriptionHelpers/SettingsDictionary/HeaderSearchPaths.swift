f719af4c1ee4d499c0af2ef5b252670a11ecd5d4
Hamp <yongbeomkwak@gmail.com>
2023-09-25 11:38:50 +0900

    :zap: :: 구글로그인 테스트 성공

--- Tuist/ProjectDescriptionHelpers/SettingsDictionary/HeaderSearchPaths.swift
+++ Tuist/ProjectDescriptionHelpers/SettingsDictionary/HeaderSearchPaths.swift
@@ -5,4 +5,19 @@
 //  Created by yongbeomkwak on 2023/09/25.
 //
 
-import Foundation
+import ProjectDescription
+
+// Header 유실을 방지하기위한 셋팅
+
+public extension SettingsDictionary {
+    static let headerSearchPaths: SettingsDictionary = [
+        "HEADER_SEARCH_PATHS": [
+            "$(inherited)",
+            "$(SRCROOT)/../../Tuist/Dependencies/SwiftPackageManager/.build/checkouts/GTMAppAuth/GTMAppAuth/Sources/Public/GTMAppAuth",
+            "$(SRCROOT)/../../Tuist/Dependencies/SwiftPackageManager/.build/checkouts/gtm-session-fetcher/Sources/Core/Public"
+        ],
+        "OTHER_LDFLAGS" : "-ObjC"
+    ]
+
+
+}
