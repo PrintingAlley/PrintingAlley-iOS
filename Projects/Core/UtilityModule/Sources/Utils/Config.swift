42aefed1f7991e0b91b96e80fb451e2e40475117
Hamp <yongbeomkwak@gmail.com>
2023-09-27 10:54:34 +0900

    :zap: :: 디버그 로그 및 hexCode를 사용한 Color 함수

--- Projects/Core/UtilityModule/Sources/Utils/Config.swift
+++ Projects/Core/UtilityModule/Sources/Utils/Config.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import UIKit
 
 public func CONFIG() -> [String:Any] {
     
@@ -38,4 +38,28 @@ public func KAKAO_NATIVE_KEY() -> String {
     
     CONFIG()["KAKAO_NATIVE_KEY"] as? String ?? ""
     
+}
+
+
+// use: colorFromRGB(0xffffff)
+public func colorFromRGB(_ rgbValue: UInt, alpha: CGFloat = 1.0) -> UIColor {
+    return UIColor(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                   green: CGFloat((rgbValue & 0xFF00) >> 8) / 255.0,
+                   blue: CGFloat(rgbValue & 0xFF) / 255.0, alpha: alpha)
+}
+
+// use: colorFromRGB("ffffff")
+public func colorFromRGB(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+    let hexToInt = UInt32(Float64("0x" + hexString) ?? 0)
+    return UIColor(red: CGFloat((hexToInt & 0xFF0000) >> 16) / 255.0,
+                   green: CGFloat((hexToInt & 0xFF00) >> 8) / 255.0,
+                   blue: CGFloat(hexToInt & 0xFF) / 255.0, alpha: alpha)
+}
+
+public func DEBUG_LOG(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
+    #if DEV
+        let fileName = file.split(separator: "/").last ?? ""
+        let funcName = function.split(separator: "(").first ?? ""
+        print("[\(fileName)] \(funcName)(\(line)): \(msg)")
+    #endif
 }
