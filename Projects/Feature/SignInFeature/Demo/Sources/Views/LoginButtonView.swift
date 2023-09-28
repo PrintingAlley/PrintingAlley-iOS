7fad0916aae89f1ae7f58153c9dfacecfc058d8a
Hamp <yongbeomkwak@gmail.com>
2023-09-28 21:48:49 +0900

    :lipstick: :: 로그인 버튼 뷰 정의

--- Projects/Feature/SignInFeature/Demo/Sources/Views/LoginButtonView.swift
+++ Projects/Feature/SignInFeature/Demo/Sources/Views/LoginButtonView.swift
@@ -9,16 +9,81 @@
 import UIKit
 import SnapKit
 import Then
+import UtilityModule
 
+public protocol LoginButtonViewDelegate: AnyObject {
+    func action(type: LoginType)
+}
 
 class LoginButtonView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var type: LoginType!
+    weak var delegate: LoginButtonViewDelegate?
+    var button: UIButton = UIButton().then {
+        $0.backgroundColor = .clear
     }
-    */
+    
+    var imageView: UIImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    var label: UILabel = UILabel()
+    
+    init(type: LoginType) {
+        super.init(frame: .zero)
+        self.type = type
+        self.preProcessing()
+        self.addSubviews()
+        self.makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
 
+    }
+    
+    @objc func event() {
+        self.delegate?.action(type: type)
+    }
+}
+
+extension LoginButtonView {
+    
+    func addSubviews() {
+        self.backgroundColor = type.color
+        self.addSubview(button)
+        self.addSubview(imageView)
+        self.addSubview(label)
+    }
+    
+    func preProcessing() {
+
+        self.layer.cornerRadius = 8
+        
+        button.addTarget(self, action: #selector(event), for: .touchDown)
+
+        imageView.image = type.image
+        
+        label.text = type.text
+    }
+    
+    func makeConstraints() {
+        
+        button.snp.makeConstraints {
+            $0.left.right.top.bottom.equalToSuperview()
+        }
+        
+        label.snp.makeConstraints{
+            $0.top.bottom.equalToSuperview().inset(17)
+            $0.centerX.equalToSuperview().offset(13)
+        }
+        
+        imageView.snp.makeConstraints{
+            $0.width.height.equalTo(20)
+            $0.centerY.equalTo(label)
+            $0.right.equalTo(label.snp.left).offset(-13)
+        }
+        
+    }
+    
 }
