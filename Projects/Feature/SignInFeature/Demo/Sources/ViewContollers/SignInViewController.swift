9a8931354499c764209208fee1b0fefd0e30d802
Hamp <yongbeomkwak@gmail.com>
2023-09-28 21:49:07 +0900

    :lipstick: :: 로그인 버튼 배치

--- Projects/Feature/SignInFeature/Demo/Sources/ViewContollers/SignInViewController.swift
+++ Projects/Feature/SignInFeature/Demo/Sources/ViewContollers/SignInViewController.swift
@@ -19,13 +19,23 @@ class SignInViewController: UIViewController {
         $0.textAlignment = .center
     }
     
-    let stackView:UIStackView = UIStackView().then{
+    let stackView: UIStackView = UIStackView().then{
         $0.axis = .vertical
+        $0.spacing = 8
     }
+    
+    let kakaoButton: LoginButtonView = LoginButtonView(type: .kakao)
+    let naverButton: LoginButtonView = LoginButtonView(type: .naver)
+    let appleButton: LoginButtonView = LoginButtonView(type: .apple)
+    let googleButton: LoginButtonView = LoginButtonView(type: .google)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        kakaoButton.delegate = self
+        naverButton.delegate = self
+        appleButton.delegate = self
+        googleButton.delegate = self
         addSubviews()
         makeConstraints()
         DEBUG_LOG("HELLO")
@@ -40,6 +50,10 @@ extension SignInViewController {
     func addSubviews() {
         self.view.addSubview(versionLabel)
         self.view.addSubview(stackView)
+        self.stackView.addArrangedSubview(kakaoButton)
+        self.stackView.addArrangedSubview(naverButton)
+        self.stackView.addArrangedSubview(appleButton)
+        self.stackView.addArrangedSubview(googleButton)
         
     }
     
@@ -52,10 +66,37 @@ extension SignInViewController {
             
         }
         
-        stackView.snp.makeConstraints{
-            $0.horizontalEdges.equalToSuperview().inset(20)
-            $0.bottom.equalTo(versionLabel.snp.top).offset(55)
+        kakaoButton.snp.makeConstraints {
+            $0.height.equalTo(54)
+            $0.left.right.equalToSuperview()
         }
+        
+        naverButton.snp.makeConstraints {
+            $0.height.equalTo(54)
+            $0.left.right.equalToSuperview()
+        }
+        
+        appleButton.snp.makeConstraints {
+            $0.height.equalTo(54)
+            $0.left.right.equalToSuperview()
+        }
+        
+        googleButton.snp.makeConstraints {
+            $0.height.equalTo(54)
+            $0.left.right.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.bottom.equalTo(versionLabel.snp.top).offset(-55)
+        }
+    }
+    
+}
+
+extension SignInViewController:LoginButtonViewDelegate {
+    func action(type: LoginType) {
+        DEBUG_LOG(type)
     }
     
 }
