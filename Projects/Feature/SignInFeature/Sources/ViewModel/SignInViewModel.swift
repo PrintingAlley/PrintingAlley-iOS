70f93c015ff8e0620549dce9b76bc73545bff73e
Hamp <yongbeomkwak@gmail.com>
2023-09-29 18:57:43 +0900

    :zap: :: 카카오톡 로그인 분기 (카카오톡 설치유무)

--- Projects/Feature/SignInFeature/Sources/ViewModel/SignInViewModel.swift
+++ Projects/Feature/SignInFeature/Sources/ViewModel/SignInViewModel.swift
@@ -44,27 +44,9 @@ final class SignInViewModel: NSObject, ViewModelType {
                     // 카카오톡 실행 가능 여부 확인
                     // 카카오톡 실행 가능 여부 확인
                     if  UserApi.isKakaoTalkLoginAvailable() {
-                        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-                            if let error = error {
-                                print(error)
-                            } else {
-                                print("loginWithKakaoTalk() success.")
-
-                                // do something
-                                _ = oauthToken
-                            }
-                        }
+                        owner.loginWithKakaoTalk()
                     } else {
-                        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-                            if let error = error {
-                                print(error)
-                            } else {
-                                print("loginWithKakaoAccount() success.")
-
-                                // do something
-                                _ = oauthToken
-                            }
-                        }
+                        owner.loginWithOutKakaoTalk()
                     }
                     
                 case .naver:
@@ -84,6 +66,36 @@ final class SignInViewModel: NSObject, ViewModelType {
         
         return output
     }
+}
+
+extension SignInViewModel {
+    
+    func loginWithKakaoTalk() {
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            } else {
+                print("loginWithKakaoTalk() success.")
+
+                // do something
+                _ = oauthToken
+            }
+        }
+    }
+    
+    func loginWithOutKakaoTalk() {
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            } else {
+                print("loginWithKakaoAccount() success.")
+
+                // do something
+                _ = oauthToken
+            }
+        }
+    }
+    
 }
 
 extension SignInViewModel: NaverThirdPartyLoginConnectionDelegate {
