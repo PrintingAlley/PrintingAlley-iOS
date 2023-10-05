94025d792d1e7c92c39ed964124661bde0cf3dbc
kpk0616 <kpk0616@naver.com>
2023-10-05 11:34:13 +0900

    🧱 :: 탭바 뷰모델, 뷰컨트롤러 파일 생성

---
+++ Projects/Feature/MainTabFeature/Sources/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  MainTabFeatureInterface
+//
+//  Created by 박의서 on 2023/10/05.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Projects/Feature/MainTabFeature/Sources/ViewModel/MainTabViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabViewModel.swift
+//  MainTabFeatureInterface
+//
+//  Created by 박의서 on 2023/10/05.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
