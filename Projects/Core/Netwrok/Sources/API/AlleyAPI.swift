2f2fd7e66d7cbec97f30029042eff83b87260ceb
Hamp <yongbeomkwak@gmail.com>
2023-10-06 14:50:23 +0900

    :zap: :: Jwt 플러그인 생성

--- Projects/Core/Netwrok/Sources/API/AlleyAPI.swift
+++ Projects/Core/Netwrok/Sources/API/AlleyAPI.swift
@@ -9,3 +9,4 @@
 import Foundation
 import UtilityModule
 import Moya
+
---
+++ Projects/Core/Netwrok/Sources/Plugin/JwtPlugin.swift
@@ -0,0 +1,62 @@
+//
+//  JwtPlugin.swift
+//  Netwrok
+//
+//  Created by yongbeomkwak on 2023/10/06.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Moya
+import Foundation
+import JwtStoreInterface
+
+struct TokenDTO: Equatable, Decodable {
+    let accessToken: String
+
+}
+
+public struct JwtPlugin: PluginType {
+    private let jwtStore: any JwtStore
+
+    public init(jwtStore: any JwtStore) {
+        self.jwtStore = jwtStore
+    }
+
+    public func prepare(
+        _ request: URLRequest,
+        target: TargetType
+    ) -> URLRequest {
+        guard let jwtTokenType  = (target as? JwtAuthorizable)?.jwtStoreProperties,
+              jwtTokenType != .none
+        else { return request }
+        var req = request
+        let token = "\(getToken(type: .accessToken))"
+        req.addValue(token, forHTTPHeaderField: jwtTokenType.rawValue)
+        return req
+    }
+
+    public func didReceive(
+        _ result: Result<Response, MoyaError>,
+        target: TargetType
+    ) {
+        switch result {
+            
+        case let .success(res):
+            if let new = try? res.map(TokenDTO.self) {
+                saveToken(token: new)
+            }
+        default:
+            break
+        }
+    }
+}
+
+private extension JwtPlugin {
+    func getToken(type: JwtStoreProperties) -> String {
+            "Bearer \(jwtStore.load(property: .accessToken))"
+    }
+
+    func saveToken(token: TokenDTO) {
+        jwtStore.save(property: .accessToken, value: token.accessToken)
+    }
+}
