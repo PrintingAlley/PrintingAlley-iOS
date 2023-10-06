d5f9bbbd756cac0ddcc5f29dfa8428023979b018
kpk0616 <kpk0616@naver.com>
2023-10-06 12:01:03 +0900

    🧱 :: 탭바 데모 뷰컨트롤러 파일 생성

--- Projects/Feature/MainTabFeature/Demo/ViewControllers/MainTabBarController.swift
+++ Projects/Feature/MainTabFeature/Demo/ViewControllers/MainTabBarController.swift
@@ -2,7 +2,7 @@
 //  MainTabBarController.swift
 //  MainTabFeatureInterface
 //
-//  Created by 박의서 on 2023/10/05.
+//  Created by 박의서 on 2023/10/06.
 //  Copyright © 2023 com. All rights reserved.
 //
 
