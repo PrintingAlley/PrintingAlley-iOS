30c1c036cb3d0ffa93a52a57a6e7b2bf714a28c7
kpk0616 <kpk0616@naver.com>
2023-10-07 01:33:41 +0900

    🧱 :: UserDefault 에 탭 시작 페이지 키값 추가

--- Projects/Feature/MainTabFeature/Demo/Sources/ViewControllers/MainTabBarController.swift
+++ Projects/Feature/MainTabFeature/Demo/Sources/ViewControllers/MainTabBarController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import MyPageFeature
+import Utili
 
 public class MainTabBarController: UITabBarController {
-
+    
+    private lazy var tabbarControllers: [UIViewController] = {
+        let viewControllers = [
+            myPageComponent.makeView().wrapNavigationController
+        ]
+        return viewControllers
+    }()
+    
+    private var myPageComponent: MyPageComponent!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
