163ba03e92a33c19d257457121aa9afd7902851a
Hamp <yongbeomkwak@gmail.com>
2023-10-07 12:49:06 +0900

    :syringe: :: 키체인 모듈 인터페이스 팩토리 생성

--- Projects/Shared/KeychainModule/Interface/DI/KeychainFactory.swift
+++ Projects/Shared/KeychainModule/Interface/DI/KeychainFactory.swift
@@ -7,3 +7,6 @@
 //
 
 import Foundation
+public protocol KeychainFactory {
+    var keychain: any Keychain { get }
+}
--- Projects/Shared/KeychainModule/Interface/Keychain.swift
+++ Projects/Shared/KeychainModule/Interface/Keychain.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+public protocol Keychain {
+    func save(key: String, value: String)
+    func load(key: String) -> String
+    func delete(key: String)
+}
