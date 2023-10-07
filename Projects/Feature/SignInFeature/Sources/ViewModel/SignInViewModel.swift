1ecce2f326be826cca5a285aa08e86696a44d98d
Hamp <yongbeomkwak@gmail.com>
2023-10-07 19:18:37 +0900

    :zap: :: UserInfo에 name 속성 추가

--- Projects/Feature/SignInFeature/Sources/ViewModel/SignInViewModel.swift
+++ Projects/Feature/SignInFeature/Sources/ViewModel/SignInViewModel.swift
@@ -71,7 +71,7 @@ final class SignInViewModel: NSObject, ViewModelType {
             
             guard let self else {return}
             
-            PreferenceManager.shared.setUserInfo(id: "A", platform: .apple)
+            PreferenceManager.shared.setUserInfo(id: "A", name: "DEFAULT",platform: .apple)
         })
         
         return output
