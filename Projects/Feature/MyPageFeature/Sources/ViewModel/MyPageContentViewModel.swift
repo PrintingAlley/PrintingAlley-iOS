4c826ea83a388faad1f9823fdac8bb7f229452c6
Hamp <yongbeomkwak@gmail.com>
2023-10-08 12:21:55 +0900

    :zap: :: 뷰모델 임시정의

--- Projects/Feature/MyPageFeature/Sources/ViewModel/MyPageContentViewModel.swift
+++ Projects/Feature/MyPageFeature/Sources/ViewModel/MyPageContentViewModel.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UtilityModule
+
+public enum MyPageCategory:String {
+    case notice = "공지사항"
+    case faq = "자주 묻는 질문"
+    case request = "1:1 문의하기"
+    case service = "서비스 이용약관"
+}
+
+final class MyPageContentViewModel : ViewModelType {
+ 
+    let categories:[MyPageCategory] = [.notice,.faq,.request,.service]
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
