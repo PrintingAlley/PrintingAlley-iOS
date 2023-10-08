b299f68af66c920c46528abf85c8f32ea89cce54
Hamp <yongbeomkwak@gmail.com>
2023-10-08 20:56:50 +0900

    :toolbox: :: Auth 도메인 모듈 생성

---
+++ Projects/Domain/AuthDomain/Project.swift
@@ -0,0 +1,22 @@
+import DependencyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: ModulePaths.Domain.AuthDomain.rawValue,
+    targets: [
+        .interface(module: .domain(.AuthDomain),dependencies: [
+            .domain(target: .BaseDomain,type: .interface)
+        ]),
+        .implements(module: .domain(.AuthDomain), dependencies: [
+            .domain(target: .AuthDomain, type: .interface),
+            .domain(target: .BaseDomain)
+        ]),
+        .testing(module: .domain(.AuthDomain), dependencies: [
+            .domain(target: .AuthDomain, type: .interface)
+        ]),
+        .tests(module: .domain(.AuthDomain), dependencies: [
+            .domain(target: .AuthDomain)
+        ])
+    ]
+)
