6ebe21bfb1e0b74bbaa44f838099d2b893da9ad1
Hamp <yongbeomkwak@gmail.com>
2023-10-09 19:26:28 +0900

    :sparkles: :: 임시 저장 ..

---
+++ Projects/Domain/AuthDomain/Sources/API/AuthAPI.swift
@@ -0,0 +1,20 @@
+//
+//  AuthAPI.swift
+//  AuthDomain
+//
+//  Created by yongbeomkwak on 2023/10/09.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+import AuthDomainInterface
+import BaseDomain
+
+enum AuthAPI {
+    case testGet
+}
+
+
+extension AuthAPI: AlleyAPI {
+    
+}
---
+++ Projects/Domain/AuthDomain/Sources/DataSource/RemoteAuthDataSourceImpl.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteAuthDataSourceImpl.swift
+//  AuthDomain
+//
+//  Created by yongbeomkwak on 2023/10/09.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+import BaseDomain
+
+final class RemoteAuthDataSourceImpl: BaseRemoteDataSource<>, RemoteAuthDataSource {
+    
+}
