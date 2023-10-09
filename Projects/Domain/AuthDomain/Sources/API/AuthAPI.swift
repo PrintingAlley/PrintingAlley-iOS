929332c3db2463ff9426e26f344ee4f9f3ebca53
Hamp <yongbeomkwak@gmail.com>
2023-10-09 19:42:24 +0900

    :sparkles: :: Auth API 스켈레톤

--- Projects/Domain/AuthDomain/Sources/API/AuthAPI.swift
+++ Projects/Domain/AuthDomain/Sources/API/AuthAPI.swift
@@ -9,12 +9,62 @@
 import Foundation
 import AuthDomainInterface
 import BaseDomain
+import Network
+import Moya
+import JwtStoreInterface
 
 enum AuthAPI {
     case testGet
 }
 
-
 extension AuthAPI: AlleyAPI {
+    var domain: AlleyDomain {
+        .tmp
+    }
+    
+    var urlPath: String {
+        
+        switch self {
+            
+        case .testGet:
+            return ""
+        }
+    }
+    
+    var errorMap: [Int: Network.AlleyError] {
+        switch self {
+        default:
+            return [
+                400: .badRequest,
+                401: .tokenExpired,
+                404: .notFound,
+                429: .tooManyRequest,
+                500: .internalServerError
+            ]
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .testGet:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+            
+        case .testGet:
+            return .requestPlain
+        }
+    }
+    
+    var jwtStoreProperties: JwtStoreProperties {
+        switch self {
+        case .testGet:
+            return .none
+        }
+    }
+    
     
 }
