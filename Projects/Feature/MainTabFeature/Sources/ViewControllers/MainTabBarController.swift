ca326e2306f5f7955404c1f7668f369c3f338398
kpk0616 <kpk0616@naver.com>
2023-10-10 19:44:40 +0900

    ✨ :: 메인탭바 아이템 적용

--- Projects/Feature/MainTabFeature/Sources/ViewControllers/MainTabBarController.swift
+++ Projects/Feature/MainTabFeature/Sources/ViewControllers/MainTabBarController.swift
@@ -11,6 +11,24 @@ import MyPageFeatureInterface
 
 public class MainTabBarController: UITabBarController {
     private var myPageFactory: any MyPageFactory
+    // 폰트 및 이미지 적용
+    private let tabBarItems: [UITabBarItem] = {
+        let items = [
+        UITabBarItem(title: "홈", image: UIImage(systemName: "home"), selectedImage: UIImage(systemName: "heart")),
+        UITabBarItem(title: "내 주변", image: UIImage(systemName: "home"), selectedImage: UIImage(systemName: "heart")),
+        UITabBarItem(title: "MY", image: UIImage(systemName: "home"), selectedImage: UIImage(systemName: "heart"))
+        ]
+        return items
+    }()
+    
+    private lazy var tabbarControllers: [UIViewController] = {
+        let viewControllers = [
+            myPageFactory.makeView(),
+            myPageFactory.makeView(),
+            myPageFactory.makeView()
+        ]
+        return viewControllers
+    }()
     
     init(myPageFactory: MyPageFactory) {
         self.myPageFactory = myPageFactory
@@ -21,16 +39,19 @@ public class MainTabBarController: UITabBarController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private lazy var tabbarControllers: [UIViewController] = {
-        let viewControllers = [
-            myPageFactory.makeView() // viewController 반환
-        ]
-        return viewControllers
-    }()
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.backgroundColor = .brown
+        configureUI()
+    }
+}
+
+extension MainTabBarController {
+    private func configureUI() {
+        for (index, item) in tabBarItems.enumerated() {
+            tabbarControllers[index].tabBarItem = item
+        }
+        
         setViewControllers(tabbarControllers, animated: true)
     }
 }
