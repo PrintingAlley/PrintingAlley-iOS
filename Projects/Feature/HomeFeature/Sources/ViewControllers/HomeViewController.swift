d9bc7e36fce812fa194143805b2b0e893434e8c3
kpk0616 <kpk0616@naver.com>
2023-10-12 00:35:30 +0900

    ✨ :: 홈뷰 UI 스크롤뷰 추가

--- Projects/Feature/HomeFeature/Sources/ViewControllers/HomeViewController.swift
+++ Projects/Feature/HomeFeature/Sources/ViewControllers/HomeViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 import UtilityModule
+import Then
+import SnapKit
 
 final class HomeViewController: UIViewController {
-
+    
+    private let scrollView = UIScrollView().then {
+        $0.backgroundColor = .green
+    }
+    private let contentView = UIView()
+    private let testLabel = UILabel().then {
+        $0.textColor = UIColor.setColor(.mainBlue(.blue1000))
+        $0.text = "test"
+    }
+    
     var viewModel: HomeViewModel!
 
     init(viewModel: HomeViewModel) {
@@ -24,5 +35,30 @@ final class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        makeConstraints()
+    }
+}
+
+extension HomeViewController {
+    private func addSubViews() {
+        view.addSubviews(contentView)
+        contentView.addSubviews(scrollView)
+        scrollView.addSubviews(testLabel)
+    }
+    
+    private func makeConstraints() {
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+            // bottom : superview - 탭바높이
+        }
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        testLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.leading.trailing.equalTo(contentView).inset(24)
+            $0.bottom.equalToSuperview().inset(100)
+        }
     }
 }
