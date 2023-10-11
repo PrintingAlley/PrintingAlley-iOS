6ba01f54992929f83db4ce4768d66ee9ddac72df
kpk0616 <kpk0616@naver.com>
2023-10-11 16:03:17 +0900

    ⚡️ :: 메인탭바 홈뷰 연결

---
+++ Projects/Feature/HomeFeature/Sources/ViewControllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  HomeFeatureInterface
+//
+//  Created by 박의서 on 2023/10/11.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
