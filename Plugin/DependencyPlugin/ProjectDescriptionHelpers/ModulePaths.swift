3b636afdf05446a70697743fe8d34b8941dd2586
kpk0616 <kpk0616@naver.com>
2023-10-11 10:35:31 +0900

    🧰 :: HomeFeature 모듈 생성

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -24,6 +24,7 @@ extension ModulePaths: MicroTargetPathConvertable {
 
 public extension ModulePaths {
     enum Feature: String, MicroTargetPathConvertable {
+        case HomeFeature
         case MainTabFeature
         case MyPageFeature
         case SignInFeature
---
+++ Projects/Feature/HomeFeature/Project.swift
@@ -0,0 +1,19 @@
+import DependencyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: ModulePaths.Feature.HomeFeature.rawValue,
+    targets: [
+        .interface(module: .feature(.HomeFeature)),
+        .implements(module: .feature(.HomeFeature), dependencies: [
+            .feature(target: .HomeFeature, type: .interface)
+        ]),
+        .testing(module: .feature(.HomeFeature), dependencies: [
+            .feature(target: .HomeFeature, type: .interface)
+        ]),
+        .demo(module: .feature(.HomeFeature), dependencies: [
+            .feature(target: .HomeFeature)
+        ])
+    ]
+)
