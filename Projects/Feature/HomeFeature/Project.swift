f852c2fc19c1e5f8a60197476813ed4ab0b2c885
Hamp <yongbeomkwak@gmail.com>
2023-10-12 09:20:59 +0900

    :film_projector: :: 홈 퓨처,인터페이스 베이스에 연결

--- Projects/Feature/HomeFeature/Project.swift
+++ Projects/Feature/HomeFeature/Project.swift
@@ -5,9 +5,12 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.HomeFeature.rawValue,
     targets: [
-        .interface(module: .feature(.HomeFeature)),
+        .interface(module: .feature(.HomeFeature),dependencies: [
+            .feature(target: .BaseFeature, type: .interface)
+        ]),
         .implements(module: .feature(.HomeFeature), dependencies: [
-            .feature(target: .HomeFeature, type: .interface)
+            .feature(target: .HomeFeature, type: .interface),
+            .feature(target: .BaseFeature)
         ]),
         .demo(module: .feature(.HomeFeature), dependencies: [
             .feature(target: .HomeFeature)
