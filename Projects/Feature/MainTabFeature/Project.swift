5db1ccf648e37accccfd4da34a05b59c72779276
Hamp <yongbeomkwak@gmail.com>
2023-10-12 10:16:01 +0900

    :film_projector: :: 메인 탭 퓨처 의존성 수정

--- Projects/Feature/MainTabFeature/Project.swift
+++ Projects/Feature/MainTabFeature/Project.swift
@@ -5,11 +5,15 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.MainTabFeature.rawValue,
     targets: [
-        .interface(module: .feature(.MainTabFeature)),
+        .interface(module: .feature(.MainTabFeature),dependencies: [
+            .feature(target: .BaseFeature, type: .interface)
+        ]),
         .implements(module: .feature(.MainTabFeature), dependencies: [
             .feature(target: .MainTabFeature, type: .interface),
             .feature(target: .MyPageFeature, type: .interface),
-            .feature(target: .HomeFeature, type: .interface)
+            .feature(target: .HomeFeature, type: .interface),
+            .feature(target: .NearByMeFeature, type: .interface),
+            .feature(target: .BaseFeature)
         ]),
         .demo(module: .feature(.MainTabFeature), dependencies: [
             .feature(target: .MainTabFeature)
