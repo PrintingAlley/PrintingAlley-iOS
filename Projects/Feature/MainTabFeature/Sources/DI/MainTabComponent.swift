920c64017893ee3510736a91d4cd63513187bd1b
Hamp <yongbeomkwak@gmail.com>
2023-10-12 09:45:41 +0900

    :syringe: :: NearByMe 메인 탭바에 니들 의존성 주입

--- Projects/Feature/MainTabFeature/Sources/DI/MainTabComponent.swift
+++ Projects/Feature/MainTabFeature/Sources/DI/MainTabComponent.swift
@@ -12,17 +12,19 @@ import UIKit
 import MainTabFeatureInterface
 import MyPageFeatureInterface
 import HomeFeatureInterface
+import NearByMeFeatureInterface
 
 public protocol MainTabDependency: Dependency {
     var mypageFactory: any MyPageFactory { get }
     var homeFactory: any HomeFactory { get }
+    var nearByMeFactory: any NearByMeFactory { get }
 }
 
 public final class MainTabComponent: Component<MainTabDependency>, MainTabFactory {
     public func makeView() -> UIViewController {
         MainTabBarController(
             myPageFactory: dependency.mypageFactory,
-            homeFactory: dependency.homeFactory
+            homeFactory: dependency.homeFactory,nearByMeFactory: dependency.nearByMeFactory
         )
     }
 }
