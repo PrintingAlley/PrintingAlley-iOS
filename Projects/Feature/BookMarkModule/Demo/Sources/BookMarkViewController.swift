38160525f8abd8df92c7d04b5221d5567e53540e
Hamp <yongbeomkwak@gmail.com>
2023-10-14 16:46:49 +0900

    :sparkles: :: 네비 게이션 바

--- Projects/Feature/BookMarkModule/Demo/Sources/BookMarkViewController.swift
+++ Projects/Feature/BookMarkModule/Demo/Sources/BookMarkViewController.swift
@@ -7,24 +7,60 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import DesignSystem
 
 class BookMarkViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    lazy var naviTitleView: UIView = UIView()
+    lazy var backButton:UIButton = UIButton().then {
+        
+        $0.setImage(DesignSystemAsset.Icon.back.image, for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFill
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    lazy var naviTitleLabel: AlleyLabel = AlleyLabel("북마크", textColor: .sub(.black), font: .header3, alignment: .center)
+    
+    lazy var tableView:UITableView = UITableView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        addSubviews()
+        makeConstraints()
     }
-    */
+    
+}
 
+extension BookMarkViewController {
+    func addSubviews() {
+        self.view.addSubviews(naviTitleView)
+        naviTitleView.addSubviews(backButton, naviTitleLabel)
+    }
+    
+    func makeConstraints() {
+        naviTitleView.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(15)
+            $0.height.equalTo(28)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+        
+        naviTitleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        
+        
+        
+    }
 }
