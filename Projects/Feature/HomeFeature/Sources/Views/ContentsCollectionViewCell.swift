3be09eba474d56bed9517d02c9c1c8db3eebf123
kpk0616 <kpk0616@naver.com>
2023-10-14 19:38:56 +0900

    ✨ :: 홈 컨텐츠컬렉션뷰셀 추가

---
+++ Projects/Feature/HomeFeature/Sources/Views/ContentsCollectionViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  CategoryCollectionViewCell.swift
+//  HomeFeatureInterface
+//
+//  Created by 박의서 on 2023/10/12.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import UIKit
+import Then
+import SnapKit
+import DesignSystem
+
+final class ContentsCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ContentsCollectionViewCell"
+    
+    private let image = UIImageView().then {
+        $0.image = DesignSystemAsset.Icon.bookMark.image
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let label = UILabel().then {
+        $0.text = "Default Text"
+        $0.font = .setFont(.caption2)
+        $0.textColor = .cyan
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .setColor(.sub(.white))
+        addSubviews()
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ContentsCollectionViewCell {
+    private func addSubviews() {
+        contentView.addSubviews(image, label)
+    }
+    
+    private func makeConstraints() {
+        image.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        label.snp.makeConstraints {
+            $0.top.equalTo(image.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
