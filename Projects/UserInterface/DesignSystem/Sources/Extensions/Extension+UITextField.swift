bbab5e688aeceac0bdad9d0fd964fb50e253eb70
Hamp <yongbeomkwak@gmail.com>
2023-10-16 16:10:12 +0900

    :bulb: :: 텍스트 필드 플레이스 홀드 적용 함수 주석 추가

--- Projects/UserInterface/DesignSystem/Sources/Extensions/Extension+UITextField.swift
+++ Projects/UserInterface/DesignSystem/Sources/Extensions/Extension+UITextField.swift
@@ -11,9 +11,13 @@ import UIKit
 
 extension UITextField {
     
+    ///  텍스트 필드 플레이스 홀더 설정
+    /// - Parameters:
+    ///   - text: 플레이스 홀더 텍스트
+    ///   - textColor:  색
+    ///   - font: 폰트
     public func setPlaceHolder(text:String, textColor: UIColor, font: UIFont.AlleyFontSystem) {
         
-
         self.attributedPlaceholder = NSAttributedString(string: text, attributes: [NSAttributedString.Key.foregroundColor : textColor, NSAttributedString.Key.font : font.font ])
         
     }
