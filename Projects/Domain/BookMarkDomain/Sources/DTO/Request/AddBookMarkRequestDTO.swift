227505d8ef90131eed07c02d01c00e373544a666
Hamp <yongbeomkwak@gmail.com>
2023-10-18 10:22:58 +0900

    :zap: :: 북마크 API 정의

---
+++ Projects/Domain/BookMarkDomain/Sources/DTO/Request/AddBookMarkRequestDTO.swift
@@ -0,0 +1,14 @@
+//
+//  AddBookMarkRequestDTO.swift
+//  BookMarkDomain
+//
+//  Created by yongbeomkwak on 10/18/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+
+struct AddBookMarkRequestDTO : Encodable {
+    let printShopId: Int
+    let bookmarkGroupId: Int
+}
---
+++ Projects/Domain/BookMarkDomain/Sources/DTO/Request/GenerateBookMarkGroupRequestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  AddBookMarkRequestDTO.swift
+//  BookMarkDomain
+//
+//  Created by yongbeomkwak on 10/18/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+
+struct GenerateBookMarkGroupRequestDTO : Encodable {
+    let name: String
+}
