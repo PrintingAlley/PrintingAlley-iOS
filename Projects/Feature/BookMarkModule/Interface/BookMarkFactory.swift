1803e56c3a50e11e6f42c81ecde00f4d830d9c89
Hamp <yongbeomkwak@gmail.com>
2023-10-18 14:07:50 +0900

    :zap: :: 데모 소스 복사 붙여넣기

---
+++ Projects/Feature/BookMarkModule/Interface/BookMarkFactory.swift
@@ -0,0 +1,15 @@
+//
+//  BookMarkFactory.swift
+//  BookMarkModuleInterface
+//
+//  Created by yongbeomkwak on 10/18/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+
+import UIKit
+
+public protocol BookMarkFactory {
+    func makeView() -> UIViewController
+}
+
---
+++ Projects/Feature/BookMarkModule/Sources/DI/BookMarkComponent.swift
@@ -0,0 +1,13 @@
+//
+//  BookMarkComponent.swift
+//  BookMarkModule
+//
+//  Created by yongbeomkwak on 10/18/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+import NeedleFoundation
+import BookMarkModuleInterface
+
+
