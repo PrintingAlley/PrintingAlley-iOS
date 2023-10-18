0e9f3b5a7fae77d61e4844725ec64862b3a2915b
Hamp <yongbeomkwak@gmail.com>
2023-10-18 12:07:23 +0900

    :syringe: :: 북마크 도메인 컴포넌트 정의

---
+++ Projects/Domain/BookMarkDomain/Sources/DI/BookMarkDomainComponent.swift
@@ -0,0 +1,58 @@
+//
+//  BookMarkDomainComponent.swift
+//  BookMarkDomain
+//
+//  Created by yongbeomkwak on 10/18/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+//
+import NeedleFoundation
+import BookMarkDomainInterface
+import JwtStoreInterface
+
+public protocol BookMarkDomainDependency: Dependency {
+    var jwtStoreFactory: any JwtStoreFactory { get }
+}
+
+public final class BookMarkDomainComponent: Component<BookMarkDomainDependency>, BookMarkDomainFactory {
+    public var fetchMyBookMarksUseCase:  any FetchMyBookMarksUseCase {
+        FetchMyBookMarksUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    public var addBookMarkUseCase: any AddBookMarkUseCase {
+        AddBookMarkUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    public var removeBookMarkUseCase: any RemoveBookMarkUseCase {
+        RemoveBookMarkUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    public var linkBookMarkUseCase: any LinkBookMarkUseCase {
+        LinkBookMarkUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    public var generateBookMarkUseCase: any GenerateBookMarkUseCase {
+        GenerateBookMarkUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    public var removeBookMarkGroupUseCase: any RemoveBookMarkGroupUseCase {
+        RemoveBookMarkGroupUseCaseImpl(bookMarkRepository: bookMarkRepository)
+    }
+    
+    
+    var remoteBookMarkDataSource: any RemoteBookMarkDataSource {
+        RemoteBookMarkDataSourceImpl(jwtStore: dependency.jwtStoreFactory.jwtStore)
+    }
+    
+    
+    public var bookMarkRepository: any BookMarkRepository {
+        BookMarkRepositoryImpl(
+            remoteBookMarkDataSource: remoteBookMarkDataSource
+        )
+    }
+    
+
+    
+}
+
