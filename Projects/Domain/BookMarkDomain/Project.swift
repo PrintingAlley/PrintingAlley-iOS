2338a0849e4ef634813b451936a9a8f76f143bf8
Hamp <yongbeomkwak@gmail.com>
2023-10-18 09:14:10 +0900

    :film_projector: :: 베이스 도메인 연결

--- Projects/Domain/BookMarkDomain/Project.swift
+++ Projects/Domain/BookMarkDomain/Project.swift
@@ -5,9 +5,12 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Domain.BookMarkDomain.rawValue,
     targets: [
-        .interface(module: .domain(.BookMarkDomain)),
+        .interface(module: .domain(.BookMarkDomain),dependencies: [
+            .domain(target: .BaseDomain, type: .interface)
+        ]),
         .implements(module: .domain(.BookMarkDomain), dependencies: [
-            .domain(target: .BookMarkDomain, type: .interface)
+            .domain(target: .BookMarkDomain, type: .interface),
+            .domain(target: .BaseDomain)
         ])
     ]
 )
