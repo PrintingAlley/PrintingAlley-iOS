1e615684fe4eae84de8f89d86cd70e6a8b1f32e9
kpk0616 <kpk0616@naver.com>
2023-10-19 12:24:05 +0900

    ⚡️ :: 태그도메인팩토리 정의, 인터페이스 완료

---
+++ Projects/Domain/TagDomain/Interface/DI/TagDomainFactory.swift
@@ -0,0 +1,17 @@
+//
+//  TagDomainFactory.swift
+//  TagDomainInterface
+//
+//  Created by 박의서 on 2023/10/19.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+
+// Domain : Feature와 Domain Factory 분리하기 위해 네이밍
+
+public protocol TagDomainFactory {
+    
+    var fetchTagToplevelUseCase: any FetchTagToplevelUseCase { get }
+}
+// usecase 이름과 똑같이
