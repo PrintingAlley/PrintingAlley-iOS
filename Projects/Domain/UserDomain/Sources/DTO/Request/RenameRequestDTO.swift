1dceebce11fb25ea4d7e0cc0a43bed936bd6774a
Hamp <yongbeomkwak@gmail.com>
2023-10-21 17:47:34 +0900

    :zap: :: UserAPi 정의

---
+++ Projects/Domain/UserDomain/Sources/DTO/Request/RenameRequestDTO.swift
@@ -0,0 +1,17 @@
+//
+//  RenameRequestDTO.swift
+//  UserDomain
+//
+//  Created by yongbeomkwak on 10/21/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+import UserDomainInterface
+
+struct RenameRequestDTO: Encodable {
+    
+    let name: String
+    
+    
+}
