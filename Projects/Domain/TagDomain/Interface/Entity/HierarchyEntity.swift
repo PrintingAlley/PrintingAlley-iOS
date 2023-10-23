d32d0e117b6dd2d9c1dc804380b3eaed8bab7331
Hamp <yongbeomkwak@gmail.com>
2023-10-23 18:54:34 +0900

    :zap: :: 베이스 모듈에 프린트 샾 DTO, Entity 명시

---
+++ Projects/Domain/TagDomain/Interface/Entity/HierarchyEntity.swift
@@ -0,0 +1,9 @@
+//
+//  HierarchyEntity.swift
+//  TagDomainInterface
+//
+//  Created by yongbeomkwak on 10/23/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Domain/TagDomain/Interface/UseCase/FetchHierarchyUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  FetchHierachyUseCase.swift
+//  TagDomainInterface
+//
+//  Created by yongbeomkwak on 10/23/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
