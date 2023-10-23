36e798db3520866abf67d213c6ccd71119d9009b
kpk0616 <kpk0616@naver.com>
2023-10-23 11:01:42 +0900

    ⚡️ :: 검색 레포지토리 생성

---
+++ Projects/Domain/SearchDomain/Interface/Repository/SearchRepository.swift
@@ -0,0 +1,15 @@
+//
+//  SearchRepository.swift
+//  SearchDomainInterface
+//
+//  Created by 박의서 on 2023/10/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import BaseDomainInterface
+
+public protocol SearchRepository {
+    func fetchPrintShopList() -> Single<[PrintShopListEntity]>
+}
