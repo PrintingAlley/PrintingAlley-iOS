7de90fcd920776dda22d70fc4dfd195b040e8d0c
Hamp <yongbeomkwak@gmail.com>
2023-10-26 19:24:21 +0900

    :film_projector: :: 루트와 연결

--- Projects/Feature/CategorySearchFeature/Project.swift
+++ Projects/Feature/CategorySearchFeature/Project.swift
@@ -5,12 +5,19 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.CategorySearchFeature.rawValue,
     targets: [
-        .interface(module: .feature(.CategorySearchFeature)),
+        .interface(module: .feature(.CategorySearchFeature),dependencies: [
+            .feature(target: .BaseFeature, type: .interface)
+        ]),
         .implements(module: .feature(.CategorySearchFeature), dependencies: [
-            .feature(target: .CategorySearchFeature, type: .interface)
+            .feature(target: .CategorySearchFeature, type: .interface),
+            .feature(target: .BaseFeature),
+            .domain(target: .TagDomain, type: .interface),
+            .domain(target: .SearchDomain, type: .interface),
         ]),
         .demo(module: .feature(.CategorySearchFeature), dependencies: [
-            .feature(target: .CategorySearchFeature)
+            .feature(target: .CategorySearchFeature),
+            .domain(target: .TagDomain, type: .interface),
+            .domain(target: .SearchDomain, type: .interface),
         ])
     ]
 )
