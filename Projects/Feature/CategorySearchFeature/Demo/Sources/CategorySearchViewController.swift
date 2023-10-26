d6d93a994f4595a20e22e54d5c6173875b633855
Hamp <yongbeomkwak@gmail.com>
2023-10-26 19:55:31 +0900

    :sparkles: :: 테이블 뷰 제외 자리 잡음

--- Projects/Feature/CategorySearchFeature/Demo/Sources/CategorySearchViewController.swift
+++ Projects/Feature/CategorySearchFeature/Demo/Sources/CategorySearchViewController.swift
@@ -7,24 +7,94 @@
 //
 
 import UIKit
+import DesignSystem
+import UtilityModule
+
 
 class CategorySearchViewController: UIViewController {
 
+    
+    lazy var naviTitleView: UIView = UIView()
+    lazy var backButton: UIButton = UIButton().then {
+        
+        $0.setImage(DesignSystemAsset.Icon.back.image, for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFill
+    }
+    
+    lazy var naviTitleLabel: AlleyLabel = AlleyLabel()
+    
+    lazy var filterButton: UIButton = UIButton().then {
+        $0.setTitle("필터", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .setFont(.body1)
+        
+        $0.setImage(DesignSystemAsset.Icon.filter.image, for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFit
+        $0.contentHorizontalAlignment = .center // // how to position content horizontally inside control. default is center
+        $0.semanticContentAttribute = .forceRightToLeft//<- 중요
+        $0.imageEdgeInsets = .init(top: 0, left: 4, bottom: 0, right: 0) //<- 중요
+        
+        $0.backgroundColor = DesignSystemAsset.MainBlue.blue500.color
+        $0.layer.cornerRadius = 16
+        $0.clipsToBounds = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addSubviews()
+        makeConstraints()
+        configureCommonUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureSwipeBack()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+extension CategorySearchViewController {
+    func addSubviews() {
+        self.view.addSubviews(naviTitleView,filterButton)
+        
+        naviTitleView.addSubviews(backButton,naviTitleLabel)
+        
+    }
+    
+    func makeConstraints() {
+        
+        naviTitleView.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(12)
+            $0.height.equalTo(32)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(24)
+        }
+        
+        naviTitleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        filterButton.snp.makeConstraints {
+            $0.width.equalTo(67)
+            $0.height.equalTo(32)
+            $0.top.equalTo(naviTitleView.snp.bottom).offset(16)
+            $0.right.equalToSuperview().inset(27)
+        }
+        
+    }
 }
