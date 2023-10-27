dc11bada9b8009c5d2bbd2f5aee179603cb3a980
kpk0616 <kpk0616@naver.com>
2023-10-27 17:40:10 +0900

    ⚡️ :: SearchVC input 작성

--- Projects/Feature/SearchFeatue/Sources/ViewControllers/SearchViewController+Input.swift
+++ Projects/Feature/SearchFeatue/Sources/ViewControllers/SearchViewController+Input.swift
@@ -12,7 +12,42 @@ import RxDataSources
 import UtilityModule
 
 extension SearchViewController {
-//    func bindViewDidLoad(input: SearchViewModel.Input) {
-//        input.viewDidLoad.onNext(())
-//    }
+    func bindUIEvent(input: SearchViewModel.Input) {
+        let editingDidEnd = searchBar.searchTextField.rx.controlEvent(.editingDidEnd)
+        let editingChanged = searchBar.searchTextField.rx.controlEvent(.editingChanged)
+        
+        searchBar.searchTextField.rx.text.orEmpty
+            .skip(1)
+            .distinctUntilChanged()
+            .bind(to: self.input.textString)
+            .disposed(by: disposeBag)
+        
+        editingChanged
+            .subscribe(onNext: { [weak self] _ in
+                guard let searchText = self?.searchBar.searchTextField.text else { return }
+                guard let self = self else { return }
+                
+                if searchText.isEmpty {
+                    self.changeToBeforeVC()
+                } else {
+                    print("\(searchText)")
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        editingDidEnd
+            .map { [weak self] in self?.searchBar.searchTextField.text ?? "" } // 텍스트 필드의 현재 텍스트 가져오기
+            .subscribe(onNext: { [weak self] searchText in
+                guard let self = self else { return }
+                
+                if searchText.isEmpty {
+                    print("비었다~")
+                    self.changeToBeforeVC()
+                } else {
+                    self.changeToAfterVC()
+                    print("안 비었다: \(searchText)")
+                }
+            })
+            .disposed(by: disposeBag)
+    }
 }
