9d83f8461d7fb8b92899066b8d1d1c57c9a1e107
kpk0616 <kpk0616@naver.com>
2023-10-27 17:40:20 +0900

    ⚡️ :: SearchVC output 작성

--- Projects/Feature/SearchFeatue/Sources/ViewControllers/SearchViewController+Output.swift
+++ Projects/Feature/SearchFeatue/Sources/ViewControllers/SearchViewController+Output.swift
@@ -10,14 +10,25 @@ import RxSwift
 import UIKit
 import UtilityModule
 
-extension SearchViewController {
-//    func bindTagDataSource(output: SearchViewModel.Output) {
-//
-//             output.searchDataSource
-//                 .debug("검색뷰")
-//                 .subscribe(onNext: {
-//                     DEBUG_LOG($0)
-//                 })
-//                 .disposed(by: disposeBag)
-//         }
+extension SearchViewController { // TODO: - 텍스트로 검색 결과 데이터 소스 받아오기
+    func bindDataSource(input: SearchViewModel.Input, output: SearchViewModel.Output) {
+        output.dataSource
+            .skip(1)
+            .bind(to: printingTableView.rx.items) { [weak self] (tableView, index, model) -> UITableViewCell in
+                guard let self = self else {
+                    return UITableViewCell()
+                }
+                
+                let indexPath: IndexPath = IndexPath(row: index, section: 0)
+                
+                guard let cell = printingTableView.dequeueReusableCell(withIdentifier: PrintingTableViewCell.identifier, for: indexPath) as? PrintingTableViewCell else {
+                    return UITableViewCell()
+                }
+                
+                cell.bindData(model: model)
+                
+                return cell
+            }
+            .disposed(by: disposeBag)
+    }
 }
