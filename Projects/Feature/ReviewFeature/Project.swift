6b19ca413d2f8f0ce158259c678381a7aaa54b22
Hamp <yongbeomkwak@gmail.com>
2023-10-28 18:25:38 +0900

    :film_projector: :: 리뷰 퓨처 연결

--- Projects/Feature/ReviewFeature/Project.swift
+++ Projects/Feature/ReviewFeature/Project.swift
@@ -5,9 +5,10 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.ReviewFeature.rawValue,
     targets: [
-        .interface(module: .feature(.ReviewFeature)),
+        .interface(module: .feature(.ReviewFeature),dependencies: []),
         .implements(module: .feature(.ReviewFeature), dependencies: [
-            .feature(target: .ReviewFeature, type: .interface)
+            .feature(target: .ReviewFeature, type: .interface),
+            .feature(target: .BaseFeature)
         ]),
         .demo(module: .feature(.ReviewFeature), dependencies: [
             .feature(target: .ReviewFeature)
