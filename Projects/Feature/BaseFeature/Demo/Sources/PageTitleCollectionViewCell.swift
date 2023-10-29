96818a2977bb764f1273990aa2b998eda896ebd7
Hamp <yongbeomkwak@gmail.com>
2023-10-29 18:50:10 +0900

    :sparkles: :: 페이지 뷰컨용 테이블 컬렉션 셀

---
+++ Projects/Feature/BaseFeature/Demo/Sources/PageTitleCollectionViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  PageTitleCollectionViewCell.swift
+//  BaseFeatureDemo
+//
+//  Created by yongbeomkwak on 10/29/23.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import UIKit
+import DesignSystem
+
+class PageTitleCollectionViewCell: UICollectionViewCell {
+    
+    lazy var label: AlleyLabel = AlleyLabel()
+    lazy var selectedBar: UIView = UIView()
+    
+    
+    override var isSelected: Bool {
+        didSet {
+            label.textColor = isSelected ? .black : .red
+            
+            UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
+                            self.selectedBar.layoutIfNeeded()
+                            self.selectedBar.alpha = self.isSelected ? 1 : 0
+
+            }, completion: nil)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        makeConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension PageTitleCollectionViewCell {
+    func addSubviews() {
+        self.contentView.addSubviews(label,selectedBar)
+    }
+    
+    func makeConstraints() {
+        label.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(15)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        selectedBar.snp.makeConstraints {
+            $0.height.equalTo(1)
+            $0.bottom.horizontalEdges.equalToSuperview()
+            
+        }
+    }
+    
+    func update(title: String) {
+        label.setTitle(title: title, textColor: .grey(.grey500), font: .body1)
+    }
+}
