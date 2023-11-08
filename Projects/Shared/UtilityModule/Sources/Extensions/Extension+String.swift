b18538c9fcb8a5ddfcf72b7b51262e170312a0ce
kpk0616 <kpk0616@naver.com>
2023-11-08 11:50:19 +0900

    ⚡️ :: 정규표현식 함수 유틸리티에서 제거

--- Projects/Shared/UtilityModule/Sources/Extensions/Extension+String.swift
+++ Projects/Shared/UtilityModule/Sources/Extensions/Extension+String.swift
@@ -12,21 +12,4 @@ public extension String {
     var isWhiteSpace: Bool{
               return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty
     }
-    
-    func findIntFromString(pattern: String) -> Int {
-        do {
-        let regex = try NSRegularExpression(pattern: pattern)
-            
-            if let match = regex.firstMatch(in: self, range: NSRange(self.startIndex..., in: self)) {
-                let range = Range(match.range(at: 0), in: self)
-                
-                if let resultStr = range.flatMap({ String(self[$0]) }), let extractedResult = Int(resultStr) {
-                    return extractedResult
-                }
-            }
-        } catch {
-            DEBUG_LOG("정규표현식 에러")
-        }
-        return 0
-    }
 }
