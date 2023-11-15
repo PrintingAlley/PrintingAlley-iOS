600ed163558e6a8b48ac7fec121061f70e6c2d3e
kpk0616 <kpk0616@naver.com>
2023-11-15 18:10:13 +0900

    ⚡️ :: 인쇄소 상세 Entity 추가

---
+++ Projects/Domain/BaseDomain/Interface/PrintShopDetailEntity.swift
@@ -0,0 +1,43 @@
+//
+//  PrintShopDetailEntity.swift
+//  BaseDomainInterface
+//
+//  Created by 박의서 on 2023/11/15.
+//  Copyright © 2023 com. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - PrintShop
+public struct PrintShopDetailEntity {
+    public let id: Int
+    public let name, address, phone, email: String
+    public let homepage: String
+    public let type, printType, afterProcess, businessHours, introduction: String
+    public let logoImage, backgroundImage: String
+    public let latitude, longitude: String
+    public let products: [ProductEntity]
+    
+    public init(id: Int, name: String, address: String, phone: String, email: String, homepage: String, type: String, printType: String, afterProcess: String, businessHours: String, introduction: String, logoImage: String, backgroundImage: String, latitude: String, longitude: String, products: [ProductEntity]) {
+        self.id = id
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.homepage = homepage
+        self.type = type
+        self.printType = printType
+        self.afterProcess = afterProcess
+        self.businessHours = businessHours
+        self.introduction = introduction
+        self.logoImage = logoImage
+        self.backgroundImage = backgroundImage
+        self.latitude = latitude
+        self.longitude = longitude
+        self.products = products
+    }
+    
+    public static func makeErrorEntity() -> PrintShopDetailEntity {
+        PrintShopDetailEntity(id: 0, name: "Error Entity", address: "", phone: "", email: "", homepage: "",type: "",printType: "",afterProcess: "",businessHours: "" ,introduction: "", logoImage: "", backgroundImage: "", latitude: "", longitude: "",products: [])
+    }
+}
