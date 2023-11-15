59cd4a52df4caf9e991bd6094476e58c49ae9934
kpk0616 <kpk0616@naver.com>
2023-11-15 18:48:06 +0900

    ⚡️ :: 전화버튼 전화 기능 구현

--- Projects/Feature/BaseFeature/Sources/ViewControllers/PrintShopDetailViewController/PrintShopDetailViewController+Input.swift
+++ Projects/Feature/BaseFeature/Sources/ViewControllers/PrintShopDetailViewController/PrintShopDetailViewController+Input.swift
@@ -13,20 +13,16 @@ import UtilityModule
 extension PrintShopDetailViewController {
     func bindButton() {
         callButton.rx.tap
-            .subscribe { [weak self] in
+            .subscribe(onNext: { [weak self] in
                 guard let self else { return }
-                
-                let number = printShopTmp.phone
-                DEBUG_LOG("핸드폰")
-                if let url = NSURL(string: "tel://0" + "\(number)"),
-                   
-                    //canOpenURL(_:) 메소드를 통해서 URL 체계를 처리하는 데 앱을 사용할 수 있는지 여부를 확인
-                   UIApplication.shared.canOpenURL(url as URL) {
-                    
-                    //사용가능한 URLScheme이라면 만들어둔 URL 인스턴스를 열어줍니다.
-                    UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+
+                let number = output.dataSource.value.phone
+
+                if let url = NSURL(string: "tel:" + "\(number)"),
+                   UIApplication.shared.canOpenURL(url as URL) { // URL 체계를 처리하는 데 앱을 사용할 수 있는지 여부를 확인
+                    UIApplication.shared.open(url as URL, options: [:], completionHandler: nil) // 만들어둔 URL 인스턴스를 열어줌
                 }
-            }
+            })
             .disposed(by: disposeBag)
     }
 }
