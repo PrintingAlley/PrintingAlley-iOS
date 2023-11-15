5d9b10340a5146a419e86ef69ec7ed08e215f69f
kpk0616 <kpk0616@naver.com>
2023-11-15 10:45:35 +0900

    ✨ :: 인쇄소상세 인쇄 방식 뷰 추가

--- Projects/Feature/BaseFeature/Sources/ViewControllers/PrintShopDetailViewController/PrintShopInfoViewController.swift
+++ Projects/Feature/BaseFeature/Sources/ViewControllers/PrintShopDetailViewController/PrintShopInfoViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 import SnapKit
 import Then
 import UtilityModule
-import BaseDomainInterface // entity용
+import DesignSystem
 
 final class PrintShopInfoViewController: UIViewController {
     
     let cellHeight: CGFloat = 41
     let cellCount: CGFloat = 5
     let tableViewInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    
+    private let containerScrollView = UIScrollView()
 
     private lazy var tableView = UITableView().then {
+        $0.isScrollEnabled = false
         $0.contentInset = tableViewInsets
         $0.separatorStyle = .none
         $0.backgroundColor = .white
@@ -26,6 +29,17 @@ final class PrintShopInfoViewController: UIViewController {
         $0.dataSource = self
         $0.register(PrintShopInfoTableViewCell.self, forCellReuseIdentifier: PrintShopInfoTableViewCell.identifier)
     }
+    
+    private let greyRoundView = UIView().then {
+        $0.backgroundColor = .setColor(.grey(.grey50))
+        $0.setRound(.allCorners, radius: 8)
+    }
+    
+    private let printTypeTitle = AlleyLabel("굵은글씨", textColor: .grey(.grey1000), font: .subtitle3, alignment: .left)
+    private let printTypeLabel = AlleyLabel("디지털인쇄, 옵셋인쇄", textColor: .grey(.grey500), font: .body2)
+    
+    private let afterProcessTitle = AlleyLabel("굵은글씨", textColor: .grey(.grey1000), font: .subtitle3, alignment: .left)
+    private let afterProcessLabel = AlleyLabel("후가공 내용", textColor: .grey(.grey500), font: .body2)
     
     let viewModel: PrintShopInfoViewModel!
     
@@ -55,16 +69,48 @@ final class PrintShopInfoViewController: UIViewController {
 // MARK: - UI 관련
 extension PrintShopInfoViewController {
     private func addSubviews() {
-        view.addSubviews(tableView)
+        view.addSubviews(containerScrollView)
+        containerScrollView.addSubviews(tableView, greyRoundView)
+        greyRoundView.addSubviews(printTypeTitle, printTypeLabel, afterProcessTitle, afterProcessLabel)
     }
     
     private func makeConstraints() {
+        containerScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
         tableView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(cellHeight * cellCount + tableViewInsets.top + tableViewInsets.bottom)
         }
         
+        greyRoundView.snp.makeConstraints {
+            $0.top.equalTo(tableView.snp.bottom).offset(16)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(HORIZON_MARGIN1())
+            $0.height.equalTo(250)
+            $0.bottom.equalToSuperview().inset(42) // TODO: - 텍스트 업데이트 후 높이 구해서 update
+        }
         
+        printTypeTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        printTypeLabel.snp.makeConstraints {
+            $0.top.equalTo(printTypeTitle.snp.bottom)
+            $0.leading.equalTo(printTypeTitle)
+        }
+        
+        afterProcessTitle.snp.makeConstraints {
+            $0.top.equalTo(printTypeLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(printTypeLabel)
+        }
+        
+        afterProcessLabel.snp.makeConstraints {
+            $0.top.equalTo(afterProcessTitle.snp.bottom)
+            $0.leading.equalTo(afterProcessTitle)
+        }
     }
 }
 
